bf16679213162b2850f59eb9c8f4cff5a165b375
Juan Pablo Mejía Gómez <juanpa0128j@gmail.com>
2026-05-13 14:28:30 -0500

    chore(alembic): merge heads a805015c381f and l0m1n2o3p4q5

---
+++ alembic/versions/m1n2o3p4q5r6_merge_heads_a805015c381f_and_l0m1n2o3p4q5.py
@@ -0,0 +1,23 @@
+"""merge heads a805015c381f and l0m1n2o3p4q5
+
+Revision ID: m1n2o3p4q5r6
+Revises: a805015c381f, l0m1n2o3p4q5
+Create Date: 2026-05-13 11:30:00.000000
+
+"""
+
+from typing import Sequence, Union
+
+
+revision: str = "m1n2o3p4q5r6"
+down_revision: Union[str, Sequence[str], None] = ("a805015c381f", "l0m1n2o3p4q5")
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
